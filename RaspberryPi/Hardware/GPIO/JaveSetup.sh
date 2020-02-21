2410dc31493e20498702df62a6997acd18542569
Ridley Neslon <rnelson009@stpaul.k12.mn.us>
2020-02-20 22:28:32 -0600

    Create JaveSetup.sh

---
+++ RaspberryPi/Hardware/GPIO/JaveSetup.sh
@@ -0,0 +1,10 @@
+# INSTALL wiringpi
+# wget https://project-downloads.drogon.net/wiringpi-latest.deb
+sudo dpkg -i wiringpi-latest.deb
+
+# INSTALL pi4j
+curl -sSL https://pi4j.com/install | sudo bash
+
+# headless
+# Install the deb from https://pi4j.com/download/pi4j-1.2.deb
+sudo dpkg -i pi4j-1.2.deb
